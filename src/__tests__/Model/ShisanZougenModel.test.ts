207fa3392ba31bde38cb364a1c23c557543703d3
lowkeep <window.lowkeep@gmail.com>
2025-02-25 08:39:18 +0900

    jest対応

--- src/__tests__/Model/ShisanZougenModel.test.ts
+++ src/__tests__/Model/ShisanZougenModel.test.ts
@@ -1,13 +1,7 @@
 import { addAndWrite } from "../../Model/ShisanZougenModel"
 import { ShisanZougenForm } from "../../ViewModel/ShisanZougenViewModel"
 
-test("check", () => {
-    console.log("テスト実行中！");
-}
-);
-
-
-test("check2", () => {
+test("ゼロから加算", () => {
     // テストデータ準備
     let data: ShisanZougenForm = {
         company: "",
@@ -24,5 +18,31 @@ test("check2", () => {
     addAndWrite(data);
     // 結果確認
     expect(data.amountOfAssetsFlows).toBe(1);
+    console.log("テスト実行中！");
+}
+);
+
+
+test("ゼロ以外から加算を２回", () => {
+    // テストデータ準備
+    let data: ShisanZougenForm = {
+        company: "",
+        sales: 0,
+        repurchases: 0,
+        redemptions: 0,
+        amountOfAssetsFlows: 11,
+        profitDistributions: 0,
+        amountOfManagementResults: 0,
+        comparedWithPreviousTerm: 0,
+        totalNetAssets: 0
+    };
+    // テスト対象メソッド呼び出し
+    addAndWrite(data);
+    // 結果確認
+    expect(data.amountOfAssetsFlows).toBe(12);
+    // テスト対象メソッド呼び出し
+    addAndWrite(data);
+    // 結果確認
+    expect(data.amountOfAssetsFlows).toBe(13);
 }
 );
